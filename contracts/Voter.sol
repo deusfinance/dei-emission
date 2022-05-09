d8a80ff2e883472d2aa572c45d9d9af7a0ce2312
Ali Parvizi <snparvizi75@gmail.com>
2022-05-09 17:59:57 +0430

    raw submit proposal

--- contracts/Voter.sol
+++ contracts/Voter.sol
@@ -7,22 +7,52 @@ contract Voter {
     address minter;
     address ve;
 
+    mapping(uint256 => bool) public proposedLendings; // its true if the lending id is proposed to be whitelisted
+    mapping(uint256 => int256) public lendingVotes; // lendingId => totalLendingVotes
+
     constructor(address minter_, address ve_) {
         minter = minter_;
         ve = ve_;
     }
 
+    function getTotalVotesOfLending(uint256 lendingId)
+        public
+        view
+        returns (int256)
+    {
+        return lendingVotes[lendingId];
+    }
+
+    function submitLending(uint256 lendingId) external {
+        require(
+            proposedLendings[lendingId] == false,
+            "Voter: ALREADY_SUBMITTED"
+        );
+        proposedLendings[lendingId] = true;
+    }
+
     function _vote(
+        uint256 lendingId,
         uint256 tokenId,
-        uint256[] memory pools,
-        int256[] memory weights
-    ) internal {}
+        int256 weight
+    ) internal {
+        require(proposedLendings[lendingId], "Voter: LENDING_NOT_SUBMITTED");
+        lendingVotes[lendingId] += weight;
+    }
 
     function vote(
-        uint256 tokenId,
-        uint256[] memory pools,
+        uint256 lendingId,
+        uint256[] memory tokenIds,
         int256[] memory weights
-    ) external {}
+    ) external {
+        require(
+            tokenIds.length == weights.length,
+            "Voter: TOKEN_ID_WEIGHT_MISMATCH"
+        );
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            _vote(lendingId, tokenIds[i], weights[i]);
+        }
+    }
     // function distribute(address _gauge) public {}
     // function distribute() external {}
     // function distribute(uint256 start, uint256 finish) public {}
--- test/voter.ts
+++ test/voter.ts
@@ -17,6 +17,12 @@ describe("Voter", () => {
   let voter: Voter;
   let me: SignerWithAddress;
   let deiBox: DeiBox;
+
+  // test ve token id and pool ids
+  let veTokeId: BigNumber = BigNumber.from(1);
+  let poolId1: BigNumber = BigNumber.from(1);
+  let poolId2: BigNumber = BigNumber.from(2);
+
   before(async () => {
     [me] = await ethers.getSigners();
     token = await deployTokenTest();
@@ -35,11 +41,31 @@ describe("Voter", () => {
       BigNumber.from(4 * 365 * 24 * 60 * 60)
     );
     await tx.wait();
-    expect(await ve.ownerOf(BigNumber.from(1))).to.eq(me.address);
+    expect(await ve.ownerOf(veTokeId)).to.eq(me.address);
   });
-  it("Voter should be able to vote", async () => {
-    let pools = [BigNumber.from(0), BigNumber.from(1), BigNumber.from(4)];
-    let weights = [BigNumber.from(-50), BigNumber.from(0), BigNumber.from(100)];
-    await voter.vote(1, pools, weights);
+  it("votes on lending #1 should be zero", async () => {
+    let totalVotes = await voter.getTotalVotesOfLending(poolId1);
+    expect(totalVotes).to.equal(0);
+  });
+  it("should fail to vote on lending #1 before it's submitted", async () => {
+    let voteTx = voter
+      .connect(me)
+      .vote(poolId1, [veTokeId], [BigNumber.from(100)]);
+    await expect(voteTx).to.be.revertedWith("Voter: LENDING_NOT_SUBMITTED");
+  });
+  it("should submit lending #1 for votings", async () => {
+    await voter.submitLending(poolId1);
+    let isProposed = await voter.proposedLendings(poolId1);
+    expect(isProposed).to.be.true;
+  });
+  it("should fail to submit same lending more than once", async () => {
+    let submitTx = voter.submitLending(poolId1);
+    await expect(submitTx).to.be.revertedWith("Voter: ALREADY_SUBMITTED");
+  });
+  it("should vote 100 power on lending #1", async () => {
+    let votingWeight = BigNumber.from(100);
+    await voter.vote(poolId1, [veTokeId], [votingWeight]);
+    let pool1Votes = await voter.getTotalVotesOfLending(poolId1);
+    expect(votingWeight).to.equal(pool1Votes);
   });
 });
